ae3cd7cc2a65a41a080cd5616aa8172775481ae6
Metranom <85638552+Metranom@users.noreply.github.com>
2021-06-09 22:14:43 +0300

    Add files via upload

---
+++ homework3/js/main.js
@@ -0,0 +1,24 @@
+﻿let a; let d;
+let b; let e;
+let c; let f;
+
+a = prompt("Enter to number", "");
+b = prompt("Enter to number", "");
+c = a + b;
+d = a - b;
+e = a * b;
+f = a / b;
+
+function two_numbers()
+{
+    let message = "a + b = " + c;
+    let message1 = "a - b = " + d;
+    let message2 = "a * b = " + e;
+    let message3 = "a / b = " + f;
+    console.log(message);
+    console.log(message1);
+    console.log(message2);
+    console.log(message3);
+}
+
+two_numbers();
