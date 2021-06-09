2e73d9ebca2c67eb79f568cdbe7a99506265c501
Metranom <85638552+Metranom@users.noreply.github.com>
2021-06-09 22:18:13 +0300

    Add files via upload

---
+++ homework4/homework4/js/main.js
@@ -0,0 +1,10 @@
+﻿function creatNewUser() {
+    let NewUser = {};
+    NewUser.firstName = prompt("Enter firstName", "");
+    NewUser.lastName = prompt("Enter lastname", "");
+    function getLogin() {
+        return NewUser.firstName.substr(0, 1).toLowerCase() + NewUser.lastName.toLowerCase();
+    };
+    console.log(getLogin());
+}
+creatNewUser();
